8bd26cc0266321eaf84af87fd5c123c479ad4c31
Kilikaev Victor <kv2013@gmail.com>
2022-01-20 19:06:42 +0500

    Добавил таблицу logstore_xapi_attachments

--- db/upgrade.php
+++ db/upgrade.php
@@ -147,5 +147,37 @@ function xmldb_logstore_xapi_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2022011900, 'logstore', 'xapi');
     }
 
+    if ($oldversion < 2022012000) {
+
+        // Define table logstore_xapi_attachments to be created.
+        $table = new xmldb_table('logstore_xapi_attachments');
+
+        // Adding fields to table logstore_xapi_attachments.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('eventid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('s3_url', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('s3_filename', XMLDB_TYPE_TEXT, null, null, null, null, null);
+        $table->add_field('s3_sha2', XMLDB_TYPE_CHAR, '256', null, null, null, null);
+        $table->add_field('s3_filesize', XMLDB_TYPE_INTEGER, '20', null, null, null, '0');
+        $table->add_field('s3_contenttype', XMLDB_TYPE_CHAR, '256', null, null, null, null);
+        $table->add_field('s3_etag', XMLDB_TYPE_CHAR, '256', null, null, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, null, null, '0');
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, null, null, '0');
+        $table->add_field('usermodified', XMLDB_TYPE_INTEGER, '10', null, null, null, '0');
+
+        // Adding keys to table logstore_xapi_attachments.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->add_key('xapi_attach_log_key', XMLDB_KEY_FOREIGN, array('eventid'), 'logstore_xapi_log', array('id'));
+
+        // Conditionally launch create table for logstore_xapi_attachments.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Xapi savepoint reached.
+        upgrade_plugin_savepoint(true, 2022012000, 'logstore', 'xapi');
+    }
+
+
     return true;
 }
