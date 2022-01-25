b2b39c62d38e3212b636c7969f72326086133503
Kilikaev Victor <kv2013@gmail.com>
2022-01-25 13:45:08 +0500

    v3.18.1-aplana-1.2.0

--- version.php
+++ version.php
@@ -18,8 +18,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = isset($plugin) && is_object($plugin) ? $plugin : new \stdClass();
 $plugin->component = 'logstore_xapi';
-$plugin->version = 2021111904;
-$plugin->release = 'v3.18.1-aplana-1.0.6';
+$plugin->version = 2022012500;
+$plugin->release = 'v3.18.1-aplana-1.2.0';
 $plugin->requires = 2014111000;
 $plugin->maturity = MATURITY_STABLE;
 
